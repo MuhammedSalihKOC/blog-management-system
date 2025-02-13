f9751c682487e70b9b8cbeefd70979138e78e339
Muhammed Salih KOÇ <muhammed_salih_koc@ogr.eskisehir.edu.tr>
2025-02-13 09:02:31 +0300

    Add blog creation and update features
    - Implemented separate pages for adding and editing blogs
    - Fixed author ID issue in update process

--- src/main/java/com/blog/controller/BlogController.java
+++ src/main/java/com/blog/controller/BlogController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -26,5 +27,37 @@ public class BlogController {
         List<Blog> blogs = blogService.getAll();
         model.addAttribute("blogs", blogs);
         return "blogs";
+    }
+    @GetMapping("/blogs/{id}")
+    public String listBlogs(@PathVariable int id, Model model) {
+        Blog blog = blogService.getById(id);
+        model.addAttribute("blogs", blog);
+        return "blogs";
+    }
+    @GetMapping("blogs/add")
+    public String blogAdder(Model model) {
+        model.addAttribute("blog", new Blog());
+        return "blog-add";
+    }
+    @PostMapping("/blogs/add/succeeded")
+    public String addBlog(@ModelAttribute Blog blog) {
+        blog.setAuthorId(1L);
+        blog.setCreatedAt(LocalDateTime.now());
+        blogService.add(blog);
+        return "redirect:/blogs";
+    }
+    @PostMapping("/blogs/update/{id}")
+    public String updateBlog(@PathVariable int id, @ModelAttribute Blog blog) {
+        Blog existingBlog = blogService.getById(id);
+        blog.setAuthorId(existingBlog.getAuthorId());
+        blog.setCreatedAt(LocalDateTime.now());
+        blogService.update(blog);
+        return "redirect:/blogs";
+    }
+    @GetMapping("/blogs/edit/{id}")
+    public String editBlog(@PathVariable int id, Model model) {
+        Blog blog = blogService.getById(id);
+        model.addAttribute("blog", blog);
+        return "blog-edit";
     }
 }
